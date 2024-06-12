312ad53873b3ca3719697885bc4d4ce8b4f45687
AnushaKAntony <anushakantony@gmail.com>
2024-06-12 23:54:24 +0530

    project completed

--- app.js
+++ app.js
@@ -28,6 +28,19 @@ app.post("/view",(req,res)=>{
     )
 })
 
+app.post("/search",(req,res)=>{
+    let input=req.body
+    hostellermodel.find(input).then(
+     (data)=>{
+         res.json(data)
+     }
+    ).catch(
+     (error)=>{
+         res.json(error)
+     }
+    )
+ })
+
 app.post("/delete",(req,res)=>{
     res.send("delete")
 })
